c918bc7399e9e7991ad46baa71d3ff42c9b6d8b4
wa019 <andrei_019@icloud.com>
2025-12-04 14:21:55 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+fetch("https://api.countapi.xyz/hit/wa019a.github.io/sitecounter")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("visits").textContent = data.value;
+  });
